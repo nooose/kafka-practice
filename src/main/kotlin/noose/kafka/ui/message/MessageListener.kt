25c611e35daeabdad23cb671d51f0f289c6ba3f2
nooose <hd15807@gmail.com>
2025-05-08 11:36:02 +0900

    feat: 명시적 매핑 선언

--- src/main/kotlin/noose/kafka/ui/message/MessageListener.kt
+++ src/main/kotlin/noose/kafka/ui/message/MessageListener.kt
@@ -6,18 +6,21 @@ import noose.kafka.ui.shared.MessageType
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
+import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.stereotype.Component
-import kotlin.time.Duration
-import kotlin.time.DurationUnit
-import kotlin.time.toDuration
-import kotlin.time.toJavaDuration
 
 @Component
 class MessageListener {
 
     private val log = KotlinLogging.logger {}
 
-    @KafkaListener(topics = [DEFAULT_TOPIC])
+    @KafkaListener(
+        topics = [DEFAULT_TOPIC],
+        properties = [
+            JsonDeserializer.USE_TYPE_INFO_HEADERS + ":false",
+            JsonDeserializer.VALUE_DEFAULT_TYPE + ":noose.kafka.ui.message.MessageResponse"
+        ]
+    )
     fun message(record: ConsumerRecord<String, MessageResponse>, acknowledgement: Acknowledgment) {
         val message = record.value()
 
